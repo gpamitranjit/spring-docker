7dae53b0274f7f568cade38d40178596ed21453e
Amit Patil <gpamitranjit@gmail.com>
2020-07-12 17:40:25 +0530

    updated springdemoapplication

--- src/main/java/com/amit/demo/DemoApplication.java
+++ src/main/java/com/amit/demo/DemoApplication.java
@@ -1,17 +1,29 @@
 package com.amit.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
   @RequestMapping("/")
-  public String home() {
-    return "Hello Docker containarized world!";
+  public String home(@RequestParam(name = "userName", required = false, defaultValue = "there!, ") String userName) {
+	  logger.info("[Stating] executing handler home()");
+	  logger.info(String.format("userName: %s", userName));
+	  logger.info("[End] executing handler home()");
+	  
+	  if(userName != null)
+		  return String.format("Hello %s Docker containarized world!", userName);
+   
+	  return "Hello Docker containarized world!";
   }
 
   public static void main(String[] args) {
